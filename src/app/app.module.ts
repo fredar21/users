82dec8e631ee3b64db7f21b32e3c4fbf164ebd12
Freddy Manrique <fredar21@gmail.com>
2018-01-18 08:37:33 -0500

    Creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
+import { UsersCreateComponent } from './components/users-create/users-create.component';
+import { UsersInformationComponent } from './components/users-information/users-information.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersListComponent,
+    UsersCreateComponent,
+    UsersInformationComponent
   ],
   imports: [
     BrowserModule
