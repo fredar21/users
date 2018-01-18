1d0eda08b8391184317ee15a1eb485a3fe7daa28
Freddy Manrique <fredar21@gmail.com>
2018-01-18 10:49:38 -0500

    LIst to users of firebase

--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+
+//services
+import { DataUsersService } from '../../services/data-users.service';
+
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
@@ -7,7 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersListComponent implements OnInit {
 
-  constructor() { }
+	users:any[]=[];
+
+  constructor( private dataFirebase:DataUsersService ) {
+
+  	this.dataFirebase.getUsers().subscribe(data=>{
+  
+  		for(let index in data){
+  			let user = data[index];  
+  			user.index = index;
+  			this.users.push(data[index]);		
+  		}
+
+  		console.log(this.users);
+  	})
+
+   }
 
   ngOnInit() {
   }
